b191c9254c3835ea7bf69257885bdb24d0469ef1
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-07-18 23:36:23 +0000

    Automatically backed up by Learn

--- spec/parrot_spec.rb
+++ spec/parrot_spec.rb
@@ -1,4 +1,8 @@
-require_relative './spec_helper'
+def parrot_noise(Squawk!)
+  puts "Squak!"
+end
+
+equire_relative './spec_helper'
 require_relative '../parrot.rb' # Code your solution in this file
 
 describe '#parrot' do
