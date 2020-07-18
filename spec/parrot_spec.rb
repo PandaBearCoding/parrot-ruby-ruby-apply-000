1bf2016e122c70f488ec72981029bf2fe53968f2
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-07-18 23:39:27 +0000

    Automatically backed up by Learn

--- spec/parrot_spec.rb
+++ spec/parrot_spec.rb
@@ -1,20 +1,10 @@
-def bird_call(parrot = "Squawk")
+def phrase(parrot = "Squawk")
   puts "#{parrot}!"
 end
 
-equire_relative './spec_helper'
-require_relative '../parrot.rb' # Code your solution in this file
 
-describe '#parrot' do
-  it 'should output "Squawk!" to the terminal when called without any arguments' do
-    expect($stdout).to receive(:puts).with("Squawk!")
-    parrot
-  end
 
-  it 'should return the default phrase, "Squawk!" when called without any arguments' do
-    phrase = parrot
-    expect(phrase).to eq("Squawk!")
-  end
+
 
   it 'should output the given phrase when called with an argument' do
     expect($stdout).to receive(:puts).with("Pretty bird!")
