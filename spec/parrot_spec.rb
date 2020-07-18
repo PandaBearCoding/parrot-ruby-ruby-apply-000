6ac7563a325f98e41dc0fcd61c170ae87d4e0a2b
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-07-18 23:40:21 +0000

    Automatically backed up by Learn

--- spec/parrot_spec.rb
+++ spec/parrot_spec.rb
@@ -1,19 +1,3 @@
-def phrase(parrot = "Squawk")
-  puts "#{parrot}!"
-end
-
-
-
-
-
-  it 'should output the given phrase when called with an argument' do
-    expect($stdout).to receive(:puts).with("Pretty bird!")
-    parrot("Pretty bird!")
-  end
-
-  it 'should return the given phrase when called with an argument' do
-    phrase = parrot("Pretty bird!")
-
-    expect(phrase).to eq("Pretty bird!")
-  end
+def phrase(parrot = "pretty bird", noise = Squawk)
+  puts "If I call the parrot a #{parrot}, it will #{noise}!"
 end
